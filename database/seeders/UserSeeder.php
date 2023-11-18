fffeafb135875b95f849d68313b256a6675c2c10
farisfaikar <farisfaikar.r@gmail.com>
2023-11-18 23:13:49 +0700

    feat: add default admin credentials to seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'remember_token' => str()->random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
