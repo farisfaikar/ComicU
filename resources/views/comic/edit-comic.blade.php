1bab5af85c4047792843fb5eeb654250d2e9cccf
farisfaikar <farisfaikar.r@gmail.com>
2023-11-20 19:59:38 +0700

    frontend: edit-comic submit button

--- resources/views/comic/edit-comic.blade.php
+++ resources/views/comic/edit-comic.blade.php
@@ -34,7 +34,7 @@
                 </select>
             </div>
 
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button type="submit" class="focus:outline-none text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
                 Submit
             </button>
         </form>
