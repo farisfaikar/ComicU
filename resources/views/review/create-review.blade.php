d4bdb33109e2e94dec65428f57bfa67c94f3193a
iskandar29 <iskadarxrpl2018smkrus@gmail.com>
2023-11-21 23:04:28 +0700

    feat: fix-reviews

--- resources/views/review/create-review.blade.php
+++ resources/views/review/create-review.blade.php
@@ -7,14 +7,25 @@
             @csrf
 
             <div class="mb-6">
-                <label for="inputUser" class="block mb-2 text-md font-large text-gray-900 dark:text-white">User</label>
-                <textarea id="inputUser_id" name="user_id" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter user here" required></textarea>
+                <label for="inputUser" class="block mb-2 text-md font-large text-neutral-900 dark:text-white">User</label>
+                <select id="inputUser_id" name="user_id" class="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="" disabled selected>Choose a user</option>
+                    @foreach ($users as $user)
+                        <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endforeach
+                </select>
+            </div>
 
-            </div>
             <div class="mb-6">
-                <label for="inputComic_id" class="block mb-2 text-md font-large text-gray-900 dark:text-white">Comic</label>
-                <textarea id="inputComic_id" name="comic_id" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter comic here" required></textarea>
+                <label for="inputComic" class="block mb-2 text-md font-large text-neutral-900 dark:text-white">Comik</label>
+                <select id="inputComic_id" name="comic_id" class="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="" disabled selected>Choose a comic</option>
+                    @foreach ($comics as $comic)
+                        <option value="{{ $comic->id }}">{{ $comic->comic_name }}</option>
+                    @endforeach
+                </select>
             </div>
+            
             <div class="mb-6">
                 <label for="inputTitle" class="block mb-2 text-md font-large text-gray-900 dark:text-white">Title</label>
                 <input type="text" name="title" id="inputTitle" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter title here" required>
@@ -24,9 +35,15 @@
                 <input type="text" name="content" min="0" id="inputContent" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter content here" required>
             </div>
             <div class="mb-6">
-                <label for="inputStars" class="block mb-2 text-md font-large text-gray-900 dark:text-white">Stars</label>
-                <input type="text" name="stars" min="0" id="inputStars" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter stars here" required>
-            </div>
+    <label for="inputStars" class="block mb-2 text-md font-large text-gray-900 dark:text-white">Stars</label>
+    <select name="stars" id="inputStars" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+        @for ($i = 1; $i <= 5; $i++)
+            <option value="{{ $i }}" {{ isset($review) && $review->stars == $i ? 'selected' : '' }}>{{ $i }} Stars</option>
+        @endfor
+    </select>
+</div>
+
+
       
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Submit
