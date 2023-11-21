bbe8be8ab9b3d2419828f0be19fab8e3748e44e2
farisfaikar <farisfaikar.r@gmail.com>
2023-11-21 16:14:18 +0700

    feat: fix database migration and seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Comic;
@@ -18,7 +19,7 @@ class ComicSeeder extends Seeder
         $comic->synopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacinia tellus eros, sit amet pharetra elit luctus eu. Proin in orci quis tortor dictum aliquet. Suspendisse potenti. Cras consectetur eros malesuada sagittis placerat. In sed pellentesque lacus. Proin malesuada lacus et leo eleifend, eget imperdiet arcu feugiat. Cras accumsan sodales ultrices. Integer pellentesque vel vulputate erat consectetur, sed congue nunc posuere. Proin congue orci dolor, vel semper tellus lobortis et. Suspendisse ultrices neque at pellentesque commodo. Proin eu ipsum sodales, facilisis libero et, consectetur ex.";
         $comic->author = "Hajime";
         $comic->stock = 200;
-        $comic->category_id = 1;
+        $comic->category_id = Category::inRandomOrder()->first()->id;
         $comic->save();
 
         Comic::factory(10)->create();
