ba1bf4c8682fc1d27c357f8dd6a08f8c4849436f
farisfaikar <farisfaikar.r@gmail.com>
2023-11-21 16:03:10 +0700

    feat: ad gap-2 to edit and delete buttons

--- resources/views/comic/index-comic.blade.php
+++ resources/views/comic/index-comic.blade.php
@@ -31,7 +31,7 @@
                             <td class="text-center"> {{ $comic->stock }}</td>
                             <td class="text-center"> {{ $comic->category_id }}</td>
                             <td class="text-center">
-                                <div class="flex flex-col md:flex-row justify-center items-center">
+                                <div class="flex flex-col gap-2 md:flex-row justify-center items-center">
                                     <a href="{{ route('comic.edit', $comic->id) }}" type="button" class="text-gray-900 hover:text-white hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
                                         Edit
                                     </a>
