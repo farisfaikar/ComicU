040c0e1939ea9ad8b8313194fccbb8a0272af0e8
iskandar29 <iskadarxrpl2018smkrus@gmail.com>
2023-11-27 23:18:56 +0700

    feat: add transaction crud

--- resources/views/transaction/create-transaction.blade.php
+++ resources/views/transaction/create-transaction.blade.php
@@ -1,47 +1,36 @@
 <x-app-layout>
-    <h1 class="text-2xl text-white">Create Transaction Page</h1>
+    <div class="flex flex-col items-center gap-5 w-full mt-10 p-5">
+        <h2 class="text-2xl font-bold">
+            Create Transaction
+        </h2>
+        <form method="post" action="{{ route('transaction.store') }}" enctype="multipart/form-data" class="w-full sm:w max-w-2xl">
+            @csrf
 
-    @error
-        <div class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-neutral-800 dark:text-red-400" role="alert">
-            <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-            </svg>
-            <span class="sr-only">Error</span>
-            <div>
-                <span class="font-medium">Ensure that these requirements are met:</span>
-                <ul class="mt-1.5 list-disc list-inside">
-                    <li>At least 10 characters (and up to 100 characters)</li>
-                    <li>At least one lowercase character</li>
-                    <li>Inclusion of at least one special character, e.g., ! @ # ?</li>
-                </ul>
+            <div class="mb-6">
+                <label for="inputUser" class="block mb-2 text-md font-large text-neutral-900 dark:text-white">User</label>
+                <select id="inputUser_id" name="user_id" class="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="" disabled selected>Choose a user</option>
+                    @foreach ($users as $user)
+                        <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endforeach
+                </select>
             </div>
-        </div>
-    @enderror
 
-    <form method="get" href="{{ route('transaction.store') }}">
-        @csrf
-        
-        <div class="mb-6">
-            <label for="userSelect" class="block mb-2 text-sm font-medium text-neutral-900 dark:text-white">Select User</label>
-            <select id="userSelect" name="user_id" class="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                <option value="" selected disabled>Select User</option>
-                @foreach ($users as $user)
-                    <option value="{{ $user->id }}">{{ $user->name }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="mb-6">
-            <label for="comicSelect" class="block mb-2 text-sm font-medium text-neutral-900 dark:text-white">Select Comic</label>
-            <select id="comicSelect" name="comic_id" class="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                <option value="" selected disabled>Select Comic</option>
-                @foreach ($comics as $comic)
-                    <option value="{{ $comic->id }}">{{ $comic->comic_name }}</option>
-                @endforeach
-            </select>
-        </div>
+            <div class="mb-6">
+                <label for="inputComic" class="block mb-2 text-md font-large text-neutral-900 dark:text-white">Comic</label>
+                <select id="inputComic_id" name="comic_id" class="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="" disabled selected>Choose a comic</option>
+                    @foreach ($comics as $comic)
+                        <option value="{{ $comic->id }}">{{ $comic->comic_name }}</option>
+                    @endforeach
+                </select>
+            </div>
 
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Create
-        </button>
-    </form>
+            
+
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Submit
+            </button>
+        </form>
+    </div>
 </x-app-layout>
