e8edcc883e613cba8331c900bd85bbb04c63749a
farisfaikar <farisfaikar.r@gmail.com>
2023-12-02 17:36:49 +0700

    style: sort tailwind classes using headwind

--- resources/views/comic/index-comic.blade.php
+++ resources/views/comic/index-comic.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
     <section class="p-5">
-        <div class="flex sm:flex-row flex-col gap-2 justify-between items-center w-full">
+        <div class="flex flex-col items-center justify-between w-full gap-2 sm:flex-row">
             <h2 class="text-2xl font-bold text-center sm:text-left">Comic List</h2>
-            <div class="flex sm:flex-row flex-col items-center align-center gap-5 sm:w-auto w-full justify-end">
+            <div class="flex flex-col items-center justify-end w-full gap-5 sm:flex-row align-center sm:w-auto">
                 <form action="{{ route('comic.search') }}" method="get" class="w-full sm:w-auto">
                     <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                    <div class="flex gap-2 justify-end">
-                        <input type="search" name="search" id="search" class="w-full sm:w-auto bg-black input input-bordered mr-1" placeholder="Search" required>
+                    <div class="flex justify-end gap-2">
+                        <input type="search" name="search" id="search" class="w-full mr-1 bg-black sm:w-auto input input-bordered" placeholder="Search" required>
                         <button type="submit" class="text-red-700 hover:text-white border border-blue-700 hover:bg-black-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-b-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-900">Search</button>
                     </div>
                 </form>
@@ -16,7 +16,7 @@
             </div>
         </div>
 
-        <div class="overflow-x-auto mt-5">
+        <div class="mt-5 overflow-x-auto">
             <table class="table table-auto">
                 <!-- head -->
                 <thead class="text-xs text-gray-300 uppercase bg-neutral-950">
@@ -30,7 +30,7 @@
                         <th class="text-center"></th>
                     </tr>
                 </thead>
-                <tbody class="bg-neutral-900 border-b border-neutral-900">
+                <tbody class="border-b bg-neutral-900 border-neutral-900">
                     @forelse ($comics as $key => $comic)
                         <tr class="border-b border-neutral-800">
                             <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"> {{ $comics->firstitem() + $key }} </td>
@@ -46,7 +46,7 @@
                                 @endif
                             </td>
                             <td class="text-center">
-                                <div class="flex flex-col gap-2 md:flex-row justify-center items-center">
+                                <div class="flex flex-col items-center justify-center gap-2 md:flex-row">
                                     <a href="{{ route('comic.edit', $comic->id) }}" type="button" class="text-blue-700 hover:text-blue-500 border border-blue-500 hover:bg-gray-900 focus:ring-4 focus:outline-none  focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-gray-800">
                                         Edit
                                     </a>
@@ -61,19 +61,19 @@
                         </tr>
                         <!-- Main modal -->
                         <div id="static-modal-{{ $loop->iteration }}" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                            <div class="relative p-4 w-full max-w-2xl max-h-full">
+                            <div class="relative w-full max-w-2xl max-h-full p-4">
                                 <!-- Modal content -->
                                 <div class="relative bg-white rounded-lg shadow dark:bg-neutral-700">
                                     <!-- Modal header -->
-                                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-neutral-600">
+                                    <div class="flex items-center justify-between p-4 border-b rounded-t md:p-5 border-neutral-600">
                                         <h3 class="text-xl font-semibold text-error">
                                             Warning
                                         </h3>
-                                        <button type="button" class="text-white bg-transparent rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-neutral-600 dark:hover:text-white" data-modal-hide="static-modal-{{ $loop->iteration }}">X
+                                        <button type="button" class="inline-flex items-center justify-center w-8 h-8 text-sm text-white bg-transparent rounded-lg ms-auto dark:hover:bg-neutral-600 dark:hover:text-white" data-modal-hide="static-modal-{{ $loop->iteration }}">X
                                         </button>
                                     </div>
                                     <!-- Modal body -->
-                                    <div class="p-4 md:p-5 space-y-4">
+                                    <div class="p-4 space-y-4 md:p-5">
                                         <p class="text-base leading-relaxed text-white">
                                             Are you sure you want to delete this comic data?
                                         </p>
@@ -83,7 +83,7 @@
                                         <form action="{{ route('comic.destroy', $comic->id) }}" method="post">
                                             @method('delete')
                                             @csrf
-                                            <button data-modal-hide="static-modal-{{ $loop->iteration }}" type="submit" class="btn btn-error text-white">Yes, I'm sure</button>
+                                            <button data-modal-hide="static-modal-{{ $loop->iteration }}" type="submit" class="text-white btn btn-error">Yes, I'm sure</button>
                                             <button data-modal-hide="static-modal-{{ $loop->iteration }}" type="button" class="btn btn-ghost">No, cancel</button>
                                         </form>
                                     </div>
