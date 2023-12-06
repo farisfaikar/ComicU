f97b37fbd1b57adac12011e2a4a90c8d911b7cfa
farisfaikar <farisfaikar.r@gmail.com>
2023-12-06 19:40:19 +0700

    style: headwind magic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,13 @@
 <x-guest-layout>
-    <div class="relative min-h-screen bg-dots-darker bg-center selection:bg-neutral-500 selection:text-white">
-        <div class="max-w-7xl mx-auto my-12 p-6 lg:p-8">
+    <div class="relative min-h-screen bg-center bg-dots-darker selection:bg-neutral-500 selection:text-white">
+        <div class="p-6 mx-auto my-12 max-w-7xl lg:p-8">
             <div class="flex justify-center">
                 <div id="default-carousel" class="relative w-full" data-carousel="slide">
                     <!-- Carousel wrapper -->
                     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
                         <div class="relative h-56 overflow-hidden rounded-lg md:h-96 ">
                             <!-- Item 1 -->
-                            <div class="hidden duration-700 ease-in-out  " data-carousel-item>
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
                                 <img src="https://images.tokopedia.net/img/cache/1208/NsjrJu/2023/5/25/b29498e6-da76-443c-97ea-426b4a4e2e01.jpg.webp?ect=4g" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-2xl" alt="...">
                             </div>
                             <!-- Item 2 -->
@@ -29,7 +29,7 @@
                         </div>
                     </div>
                     <!-- Slider indicators -->
-                    <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                    <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2 rtl:space-x-reverse">
                         <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
                         <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
                         <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
@@ -37,7 +37,7 @@
                         <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
                     </div>
                     <!-- Slider controls -->
-                    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                    <button type="button" class="absolute top-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer start-0 group focus:outline-none" data-carousel-prev>
                         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
@@ -45,7 +45,7 @@
                             <span class="sr-only">Previous</span>
                         </span>
                     </button>
-                    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                    <button type="button" class="absolute top-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer end-0 group focus:outline-none" data-carousel-next>
                         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
@@ -55,9 +55,9 @@
                     </button>
                 </div>
             </div>
-            <div class="mt-10 grid grid-cols-1 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-10">
+            <div class="grid grid-cols-1 gap-10 mt-10 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3">
                 @foreach ($comics as $comic)
-                    <div class="max-w-sm bg-neutral-900 border border-neutral-800 rounded-lg shadow">
+                    <div class="max-w-sm border rounded-lg shadow bg-neutral-900 border-neutral-800">
                         <a href="#">
                             <img class="rounded-t-lg" src="https://cdn.gramedia.com/uploads/items/9786020523316_Melangkah_UV_Spot_R4-1.jpg" alt="..." />
                         </a>
@@ -71,7 +71,7 @@
                                 {{ strlen($comic->synopsis) > 100 ? substr($comic->synopsis, 0, 100) . '...' : $comic->synopsis }}
                             </p>
 
-                            <a href="#" class="inline-flex items-center pl-3 pr-3 py-2 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
+                            <a href="#" class="inline-flex items-center py-2 pl-3 pr-3 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
                                 Add Cart
                                 <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
                                     <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
