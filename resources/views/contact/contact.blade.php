0c382e5febf2a2ead6ec171ee60ca4c3df2daa66
farisfaikar <farisfaikar.r@gmail.com>
2023-12-08 18:23:13 +0700

    fe: add mb-6 to h2

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -2,54 +2,56 @@
     <div class="relative min-h-screen my-12 bg-center bg-dots-darker selection:bg-neutral-500 selection:text-white">
         <div class="flex flex-col gap-5 p-6 mx-auto mt-12 max-w-7xl lg:p-8">
             <div class="flex justify-start px-5 py-10 rounded-lg bg-neutral-900">
-            <div class="container my-24 mx-auto md:px-6">
-  <!-- Section: Design Block -->
-  <section class="mb-32">
-    <div class="flex flex-wrap">
-      <div class="mb-10 w-full shrink-0 grow-0 basis-auto md:mb-0 md:w-6/12 md:px-3 lg:px-6">
-        <h2 class="text-4xl font-extrabold text-neutral-100 dark:text-white sm:text-5xl">Contact us</h2>
-        <p class="mb-6 text-neutral-500 dark:text-neutral-300">
-        ComicU is a comic shop online management website application created to provide a smooth comic inventory management system. ComicU has various CMS (Content Management System) features including: Comic book management CMS to manage comic book inventory, graphs to view comic sales and inventory in and out, user and employee account management, comic review, transaction, and order management, and others.
-        </p>
-        <p class="mb-2 text-neutral-500 dark:text-neutral-300">
-          Semarang, 94126, Indonesia
-        </p>
-        <p class="mb-2 text-neutral-500 dark:text-neutral-300">
-        +62 895-3962-61290
-        </p>
-        <p class="mb-2 text-neutral-500 dark:text-neutral-300">
-          comicu@gmail.com
-        </p>
-      </div>
-      <div class="mb-12 w-full shrink-0 grow-0 basis-auto md:mb-0 md:w-6/12 md:px-3 lg:px-6">
-      <form action="{{ route('contact.store') }}" method="post">
-    @csrf
-    <div class="relative mb-6" data-te-input-wrapper-init>
-        <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="name" id="exampleInput90" placeholder="Name" />
-    </div>
-    <div class="relative mb-6" data-te-input-wrapper-init>
-        <input type="email" name="email" id="exampleInput91" placeholder="Email address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-    </div>
-    <div class="relative mb-6" data-te-input-wrapper-init>
-        <textarea name="message" id="exampleFormControlTextarea1" rows="3" placeholder="Your message" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
-    </div>
-    <div class="mb-6 inline-block min-h-[1.5rem] justify-center pl-[1.5rem] md:flex">
-        <input class="relative float-left mt-[0.15rem] mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer `hover`:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]" type="checkbox" value="" id="exampleCheck96" checked />
-        <label class="inline-block pl-[0.15rem] hover:cursor-pointer text-neutral-500" for="exampleCheck96">
-            Send me a copy of this message
-        </label>
-    </div>
-    <button type="submit" data-te-ripple-init data-te-ripple-color="light" class="mb-6 inline-block w-full rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
-        Send
-    </button>
-</form>
+                <div class="container my-24 mx-auto md:px-6">
+                    <!-- Section: Design Block -->
+                    <section class="mb-32">
+                        <div class="flex flex-wrap">
+                            <div class="mb-10 w-full shrink-0 grow-0 basis-auto md:mb-0 md:w-6/12 md:px-3 lg:px-6">
+                                <h2 class="text-4xl font-extrabold text-neutral-100 dark:text-white sm:text-5xl mb-6">Contact us</h2>
+                                <p class="mb-6 text-neutral-500 dark:text-neutral-300">
+                                    ComicU is a comic shop online management website application created to provide a smooth comic inventory management system. ComicU has various CMS (Content Management System) features including: Comic book management CMS to manage comic book inventory, graphs to view comic sales and inventory in and out, user and employee account management, comic review, transaction, and order management, and others.
+                                </p>
+                                <p class="mb-2 text-neutral-500 dark:text-neutral-300">
+                                    Semarang, 94126, Indonesia
+                                </p>
+                                <p class="mb-2 text-neutral-500 dark:text-neutral-300">
+                                    +62 895-3962-61290
+                                </p>
+                                <p class="mb-2 text-neutral-500 dark:text-neutral-300">
+                                    comicu@gmail.com
+                                </p>
+                            </div>
+                            <div class="mb-12 w-full shrink-0 grow-0 basis-auto md:mb-0 md:w-6/12 md:px-3 lg:px-6">
+                                <form action="{{ route('contact.store') }}" method="post">
+                                    @csrf
+                                    <div class="relative mb-6" data-te-input-wrapper-init>
+                                        <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="name" id="exampleInput90" placeholder="Name" />
+                                    </div>
+                                    <div class="relative mb-6" data-te-input-wrapper-init>
+                                        <input type="email" name="email" id="exampleInput91" placeholder="Email address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                    </div>
+                                    <div class="relative mb-6" data-te-input-wrapper-init>
+                                        <textarea name="message" id="exampleFormControlTextarea1" rows="3" placeholder="Your message" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+                                    </div>
+                                    <div class="mb-6 inline-block min-h-[1.5rem] justify-center pl-[1.5rem] md:flex">
+                                        <input
+                                            class="relative float-left mt-[0.15rem] mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer `hover`:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                                            type="checkbox" value="" id="exampleCheck96" checked />
+                                        <label class="inline-block pl-[0.15rem] hover:cursor-pointer text-neutral-500" for="exampleCheck96">
+                                            Send me a copy of this message
+                                        </label>
+                                    </div>
+                                    <button type="submit" data-te-ripple-init data-te-ripple-color="light" class="mb-6 inline-block w-full rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+                                        Send
+                                    </button>
+                                </form>
 
-      </div>
-    </div>
-  </section>
-  <!-- Section: Design Block -->
-</div>
-<!-- Container for demo purpose -->
+                            </div>
+                        </div>
+                    </section>
+                    <!-- Section: Design Block -->
+                </div>
+                <!-- Container for demo purpose -->
             </div>
         </div>
     </div>
